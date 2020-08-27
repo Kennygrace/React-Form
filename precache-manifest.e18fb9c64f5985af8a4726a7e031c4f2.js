7ad7053555365ada5f5d89f4bb8d50d45adb8fe4
kennygrace <igekehinde90@gmail.com>
2020-08-27 13:09:57 +0100

    Updates

---
+++ precache-manifest.e18fb9c64f5985af8a4726a7e031c4f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afcc136f6564f7669e3ac6630b4ee7f4",
+    "url": "/Kennygrace/React-Form/index.html"
+  },
+  {
+    "revision": "9b167090b760d919c97d",
+    "url": "/Kennygrace/React-Form/static/js/2.729ecec2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Kennygrace/React-Form/static/js/2.729ecec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186729e555ad4ee10a86",
+    "url": "/Kennygrace/React-Form/static/js/main.43330a3a.chunk.js"
+  },
+  {
+    "revision": "e9f30bb94adb4eb10baa",
+    "url": "/Kennygrace/React-Form/static/js/runtime-main.f667bd95.js"
+  }
+]);
